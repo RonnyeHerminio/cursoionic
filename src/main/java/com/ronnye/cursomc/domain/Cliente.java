9e21ed6c97c991c4b9d01459f91fc70f9ea0c95a
Ronnye Herminio <rpherminio@hotmail.com>
2018-07-16 10:31:50 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/ronnye/cursomc/domain/Cliente.java
+++ src/main/java/com/ronnye/cursomc/domain/Cliente.java
@@ -35,6 +35,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	
 	public Cliente() {
 		
@@ -145,6 +148,16 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 	
 	
---
+++ src/main/java/com/ronnye/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.ronnye.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronnye.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
