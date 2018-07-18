5832db416f47a37bb0adc2ba4d34f48674761801
Ronnye Herminio <rpherminio@hotmail.com>
2018-07-18 13:35:07 -0300

    Configuração e envio do projeto para produção com postgresql com ddl-auto=none

--- src/main/java/com/ronnye/cursomc/config/DevConfig.java
+++ src/main/java/com/ronnye/cursomc/config/DevConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ronnye.cursomc.services.DBService;
 
 @Configuration
-@Profile("prod")
+@Profile("dev")
 public class DevConfig {
 
 	@Autowired
